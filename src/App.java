f6c2e7ba9fe4c95a309fb32c36a3e6cbca3334e7
Micah <micahgann3@gmail.com>
2025-10-20 12:06:05 -0400

    A

--- src/App.java
+++ src/App.java
@@ -7,14 +7,32 @@ public class App extends PApplet{
 
     public void setup(){
         
-    }
-
-    public void settings(){
         
     }
 
+    public void settings(){
+        size(900, 900);
+    }
+    float squareY = 800;
+    float squareX = 800;
+    
     public void draw(){
-       
-
+       background(12, 63, 143);
+       fill(255);
+        rect(790, 850, 60, -800);
+        fill(255);
+        rect(790, 50, -200, 45);
+        fill(255);
+        triangle(X, Y, X, Y, X, Y);
+        fill(12, 63, 143);
+        rect(squareX, squareY, 40, 40);
+        noStroke();
+    }
+    public void keyPressed(){
+        if (keyCode == UP) {
+            squareY = squareY - 1;
+        } else if (keyCode == DOWN) {
+            squareY = squareY + 1;
+        }
     }
 }
