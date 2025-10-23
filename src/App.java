9ab88233c5a21769778acc3d3f28aa459d6eedf1
Micah <micahgann3@gmail.com>
2025-10-23 15:54:46 -0400

    doen

--- src/App.java
+++ src/App.java
@@ -8,13 +8,15 @@ public class App extends PApplet {
     float ystart = 800;
     float xstart = 800;
     float squareStart = 803;
-    
+    float enemyX = 700;
+    float enemyY = 200;
     float squareY = 803;
     float squareX = 803;
     float x = 800;
     float y = 800;
     double squareSpeed = 2.5;
     boolean left, up, down, right;
+    
 
     public void setup() {
 
@@ -30,14 +32,17 @@ public class App extends PApplet {
         rect(790, 850, 60, -800);
         rect(790, 50, -200, 45);
         triangle(650, 50, 450, 2500, 250, 50);
-        
+
         noStroke();
         fill(0);
         rect(x, y, 40, 40);
-        
+
         fill(0, 120, 209);
         rect(squareX, squareY, 34, 34);
-
+        fill(12, 63, 143);
+        circle(enemyX, enemyY, 15);
+        enemyX = enemyX - 9;
+        
         if (left == true) {
             squareX -= squareSpeed;
             x -= squareSpeed;
@@ -55,31 +60,19 @@ public class App extends PApplet {
             x += squareSpeed;
         }
 
-        int currentColor = get((int) x, (int) y);
+        int topColor = get((int) (x + 20), (int) (y));
+        int bottomColor = get((int) (x + 20), (int) (y + 40));
+        int leftColor = get((int) (x), (int) (y + 20));
+        int rightColor = get((int) (x + 40), (int) (y + 20));
 
-        currentColor = get((int) x, (int) (y - upSize / 2));
-        if (red(currentColor) == 12 && green(currentColor) == 63 && blue(currentColor) == 143) {
-            x = xstart;
-            y = ystart;
-            squareX = squareStart;
-            squareY = squareStart;
-        }
-        currentColor = get((int) x, (int) (y + downSize / 2));
-        if (red(currentColor) == 12 && green(currentColor) == 63 && blue(currentColor) == 143) {
-            x = xstart;
-            y = ystart;
-            squareX = squareStart;
-            squareY = squareStart;
-        }
-        currentColor = get((int) (x - leftSize / 2), (int) y);
-        if (red(currentColor) == 12 && green(currentColor) == 63 && blue(currentColor) == 143) {
-            x = xstart;
-            y = ystart;
-            squareX = squareStart;
-            squareY = squareStart;
-        }
-        currentColor = get((int) (x + rightSize / 2), (int) y);
-        if (red(currentColor) == 12 && green(currentColor) == 63 && blue(currentColor) == 143) {
+        int red = 12, green = 63, blue = 143;
+        enemy();
+
+        if ((red(topColor) == red && green(topColor) == green && blue(topColor) == blue) ||
+                (red(bottomColor) == red && green(bottomColor) == green && blue(bottomColor) == blue) ||
+                (red(leftColor) == red && green(leftColor) == green && blue(leftColor) == blue) ||
+                (red(rightColor) == red && green(rightColor) == green && blue(rightColor) == blue)) {
+
             x = xstart;
             y = ystart;
             squareX = squareStart;
@@ -108,6 +101,19 @@ public class App extends PApplet {
             left = false;
         } else if (keyCode == RIGHT) {
             right = false;
+        }
+    }
+
+    public void enemy(){
+        if(enemyX <= 200){
+            enemyX = 700;
+            enemyY = 200;
+        }
+    }
+
+    public boolean checkOver(int x, int y){
+        if (checkOver(enemyX, enemyY)) {
+            
         }
     }
 }
